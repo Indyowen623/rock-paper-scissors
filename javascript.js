de91d3e25edde3f6bac626f8d04f60d2ae4c3d87
Hexappeal <150848904+Indyowen623@users.noreply.github.com>
2025-02-20 06:46:10 -0600

    Initial commit add pseudocode

---
+++ javascript.js
@@ -0,0 +1,30 @@
+/* Logic to get Computer choice */
+    /* Create function that randomly returns "rock" "paper" or "scissors" */
+        /* Create function called getComputerChoice */
+            /* Generate random number between 1-100 */
+            /* Conditions that determine what numbers return "rock" "paper" or "scissors"
+        
+/* Logic to get human choice */    
+     /* Create function to input human choice */
+        /* Create function called getHumanChoice */
+        /* Create loop that only accepts input of "Rock" "Paper" or "Scissors"
+
+/* Declare players score variables */
+    /* Create variables to keep track of score */
+        /* Create variable humanScore with initial value 0 */
+        /* Create variable computerScore with initial value 0 */
+
+/* Logic to play single round */
+    /* Create function that recieves arguments from getComputerChoice and getHumanChoice, determines a winner, and increments the winner's score 1 point.
+        /* Create function called playRound */
+        /* Create case-insensitive parameter humanChoice set to getHumanChoice */
+        /* Create case-insensitive parameter computerChoice set to getComputerChoice */
+
+    /* Declare a round winner */
+      /* Print string that declares winner */
+      /* Increment humanScore or computerScore based on the winner */
+
+/* Logic to play entire game */
+    /* Create function called playGame */
+    /* Declare playRound humanScore and computerScore */
+    /* Play 5 rounds */
